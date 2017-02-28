99a6fc52b7d96c1665c1facf13da4e656fd25405
Bradley Scott <bradley.s.scott@gmail.com>
2017-02-28 20:58:13 +1300

    Properly return 400s if match is duplicate

--- api/controllers/MatchController.js
+++ api/controllers/MatchController.js
@@ -4,8 +4,54 @@
  * @description :: Server-side logic for managing Matches
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+var moment = require('moment');
 
 module.exports = {
-	
+    create: function(req, res) {
+        sails.log.debug('Validating Match object before creation');
+
+        Match.find({
+            where: {
+                homeTeam: [req.body.homeTeam, req.body.awayTeam],
+                awayTeam: [req.body.awayTeam, req.body.homeTeam]
+            }
+        }).then(function(matches) {
+            if(matches.length == 0) {
+                sails.log.debug('No Matches between these sides before. Creating Match');
+                createMatch(req, res);
+            } else {
+                var proposedStart = moment(req.body.startDate);
+
+                var isClash = _(matches).some(function(match) {
+                    return proposedStart.isSame(match.startDate, 'day');
+                });
+
+                if(isClash) {
+                    var message = 'Can not create Match. These teams have played each other on this date previously';
+                    sails.log.debug(message);
+                    return res.badRequest(message);
+                } else { createMatch(req, res); }
+            }
+        }).catch(function(error) {
+            sails.log.error(error);
+            return res.serverError(error);
+        });
+
+    }
+};
+
+var createMatch = function(req, res) {
+    sails.log.debug('Attempting to create match...');
+
+    Match.create(req.body).exec(function(err, newMatch){
+        if(err) {
+            sails.log.error(error);
+            return res.error(error);
+        } else {
+            sails.log.debug('Match successfully created');
+            return res.ok(newMatch);
+        }
+    });
+
 };
 
--- api/models/Match.js
+++ api/models/Match.js
@@ -4,9 +4,6 @@
  * @description :: An instance where 2 Teams get together to play a Match of cricket
  * @docs        :: http://sailsjs.org/#!documentation/models
  */
-
-var moment = require('moment');
-
 module.exports = {
 
     attributes: {
@@ -29,36 +26,5 @@ module.exports = {
         limitedOvers: {
             type: 'integer'
         },
-    },
-
-    beforeCreate: function(values, cb) {
-        sails.log.debug('Validating Match object before creation');
-
-        Match.find({
-            where: {
-                homeTeam: [values.homeTeam, values.awayTeam],
-                awayTeam: [values.awayTeam, values.homeTeam]
-            }
-        }).then(function(matches) {
-            if (matches.length == 0) {
-                sails.log.debug('No Matches between these sides before. Creating Match');
-                cb();
-            } else {
-                var proposedStart = moment(values.startDate);
-
-                var isClash = _(matches).some(function(match) {
-                    return proposedStart.isSame(match.startDate, 'day');
-                });
-
-                if (isClash) { 
-                  var message = 'Can not create Match. These teams have played each other on this date previously';
-                  sails.log.debug(message);
-                  cb(message); 
-                } else { cb(); }
-            }
-        }).catch(function(error) {
-            sails.log.error(error);
-            cb(error);
-        });
-    },
+    }
 };
